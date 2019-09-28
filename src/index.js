46e038f55f123003b86d5599d69ed1fbec139928
javierfuentesm <javier.fuentesm@hotmail.com>
2019-09-28 11:18:52 -0500

    Se terminan retos

--- src/index.js
+++ src/index.js
@@ -1,38 +1,54 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://rickandmortyapi.com/api/character/";
+
+window.onload = window.localStorage.clear();
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      localStorage.setItem("next_fetch", response.info.next);
       const characters = response.results;
-      let output = characters.map(character => {
-        return `
+      let output = characters
+        .map(character => {
+          return `
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
+    `;
+        })
+        .join("");
+      let newItem = document.createElement("section");
+      newItem.classList.add("Items");
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
-}
+};
 
-const loadData = () => {
-  getData(API);
-}
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+const loadData = async () => {
+  if (localStorage.getItem("next_fetch") === null) {
+    return await getData(API);
+  } else if (localStorage.getItem("next_fetch") === "") {
+    intersectionObserver.unobserve($observe);
+    return alert("Ya no hay personajes");
+  } else {
+    return await getData(localStorage.getItem("next_fetch"));
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+};
+
+const intersectionObserver = new IntersectionObserver(
+  entries => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  },
+  {
+    rootMargin: "0px 0px 100% 0px"
+  }
+);
 
 intersectionObserver.observe($observe);
+
